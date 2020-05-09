dca96be7eea4a39c69b303d6bd272f91796fa32a
Spaceflyer234 <64615967+Spaceflyer234@users.noreply.github.com>
2020-05-09 17:07:38 -0500

    Add files via upload

---
+++ Scratch Extension.js
@@ -0,0 +1,44 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.when = function(input) {
+        return input;
+    };
+
+    ext.alert = function(text) {
+        alert(text);
+    };
+
+    ext.confirm = function(text) {
+        return confirm(text);
+    };
+
+    ext.prompt = function(text) {
+        var output = prompt(text);
+        if (!output) {
+            return '';
+        } else {
+            return output;
+        };
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['h','when %b','when'],
+            [' ','alert %s','alert'],
+            ['b','confirm %s','confirm'],
+            ['r','prompt %s','prompt'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Extension', descriptor, ext);
+})({});
